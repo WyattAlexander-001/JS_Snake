72e2adc64816f4dcc2bf1657236afb24fcd2b043
Wyatt B <wyattbushman117@gmail.com>
2022-03-07 22:47:17 -0500

    coding board

--- apps.js
+++ apps.js
@@ -1,11 +1,47 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+var snake = {
+    parts:[
+        {x: 4, y:2},
+        {x: 3, y:2},
+        {x: 2, y:2}
+    ],
+    facing: 'E'
+};
+var game = {
+    board: [
+    "###############",
+    "#             #",
+    "#             #",
+    "#             #",
+    "#    ####     #",
+    "#    ####     #",
+    "#             #",
+    "#             #",
+    "#             #",
+    "###############"
+    ]
+    };
+    
+var graphics = {
+    canvas : document.getElementById("canvas"),
+    squareSize : 30,
+    drawBoard: function() {
+        var ctx = graphics.canvas.getContext("2d");
+        var currentYoffset = 0;
+        
+        game.board.forEach(function checkLine(line) {
+        line = line.split('');
+        var currentXoffset = 0;
+        line.forEach(function checkCharacter(character) {
+            if(character == '#') {
+            ctx.fillStyle = "black";
+            ctx.fillRect(currentXoffset, currentYoffset, graphics.squareSize, graphics.squareSize);
+            }
+            currentXoffset += graphics.squareSize;
+            });
+            currentYoffset += graphics.squareSize;
+            });
+    }
+};
 
-function draw(){
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0,0,30,30)
-}
-
-function remove(){
-    ctx.clearRect(0,0,300,300)
-}
+graphics.drawBoard();
+    
